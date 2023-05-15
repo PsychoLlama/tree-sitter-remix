ba7fdb857124ac0f50abc7faf47b34a2aa1f933a
Jesse Gibson <JesseTheGibson@gmail.com>
2023-05-14 21:20:36 -0400

    Add grammar rules for primitives and assignments

--- grammar.js
+++ grammar.js
@@ -1,7 +1,22 @@
 module.exports = grammar({
   name: "remix",
 
+  word: ($) => $.identifier,
+
   rules: {
-    source_file: ($) => "hello",
+    source_file: ($) => repeat($.assignment),
+    identifier: () => /[a-z_]+/,
+    assignment: ($) =>
+      seq(field("identifier", $.identifier), "=", field("value", $._primitive)),
+
+    // Primitives
+    _primitive: ($) => choice($.string, $.number, $.boolean),
+
+    number: () => /\d+(\.\d+)?/,
+    boolean: () => choice("true", "false"),
+    string_fragment: () => /[^"\\]+/,
+    escape_sequence: () => token.immediate(/\\./),
+    string: ($) =>
+      seq('"', repeat(choice($.string_fragment, $.escape_sequence)), '"'),
   },
 });
