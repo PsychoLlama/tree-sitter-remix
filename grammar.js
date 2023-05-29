9563be3c82633aca4b5438edc3c4d93416aafc0a
Jesse Gibson <JesseTheGibson@gmail.com>
2023-05-29 12:15:33 -0400

    Add support for call expressions
    Now the language supports expressions. This include identifiers and
    literals, along with a new `call_expression` type. A call follows the
    functional convention of not requiring parentheses. Arguments or the
    callee may be the product of another expression.

--- grammar.js
+++ grammar.js
@@ -1,3 +1,8 @@
+const precedence = {
+  identifier: 1,
+  call: 2,
+};
+
 module.exports = grammar({
   name: "remix",
 
@@ -7,7 +12,11 @@ module.exports = grammar({
     source_file: ($) => repeat($.definition),
     identifier: () => /[a-zA-Z_][0-9a-zA-Z_-]*/,
     definition: ($) =>
-      seq(field("identifier", $.identifier), "=", field("value", $._primitive)),
+      seq(
+        field("identifier", $.identifier),
+        "=",
+        field("value", $._expression)
+      ),
 
     // Primitives
     _primitive: ($) => choice($.string, $.number, $.boolean),
@@ -18,5 +27,25 @@ module.exports = grammar({
     escape_sequence: () => token.immediate(/\\./),
     string: ($) =>
       seq('"', repeat(choice($.string_fragment, $.escape_sequence)), '"'),
+
+    // Expressions
+    _expression: ($) =>
+      choice(
+        $.call_expression,
+        $._primitive,
+        $._parenthesized_expression,
+        prec(precedence.identifier, $.identifier)
+      ),
+
+    _parenthesized_expression: ($) => seq("(", $._expression, ")"),
+
+    call_expression: ($) =>
+      prec(
+        precedence.call,
+        seq(
+          field("callee", choice($.identifier, $._parenthesized_expression)),
+          field("arguments", repeat1($._expression))
+        )
+      ),
   },
 });
