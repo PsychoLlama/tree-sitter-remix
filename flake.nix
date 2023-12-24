397837c08ba19b6d9d9f4be22035134c47f147a3
Jesse Gibson <JesseTheGibson@gmail.com>
2023-12-23 22:38:08 -0500

    Upgrade nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,18 +4,25 @@
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
   outputs = { self, nixpkgs, rust-overlay }:
-    let inherit (nixpkgs) lib;
+    let
+      inherit (nixpkgs) lib;
+
+      # The list of systems supported by nixpkgs and hydra.
+      defaultSystems =
+        [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+
+      # Attrs { system -> pkgs }
+      packageUniverse = lib.genAttrs defaultSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        });
+
+      eachSystem = lib.flip lib.mapAttrs packageUniverse;
 
     in {
-      devShell = lib.genAttrs lib.systems.flakeExposed (system:
-        let
-          nodejs = pkgs.nodejs-18_x;
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (import rust-overlay) ];
-          };
-
-        in pkgs.mkShell {
+      devShell = eachSystem (system: pkgs:
+        pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             rust-bin.stable.latest.default
             tree-sitter
